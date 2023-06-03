0ab8316b9a961cba24c1c35ceda43f2e815f2465
Jeje <jeremiech06@gmail.com>
2023-06-03 09:30:23 +0200

    stage the primary commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html'
+  ],
+  theme: {
+    extend: {
+      backgroundSize:{
+        'cover':'cover',
+        'center':'center',
+        'repeat':'no-repeat'
+      }
+    
+  },
+  plugins: [],
+}
+}
+
